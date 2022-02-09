fe463ec5a8853b2c1c3083e03c5dadef9053e512
Juuso Korhonen <juseek@gmail.com>
2022-02-09 12:17:53 +0200

    rename dto to nestjs-swagger supported format, add docstring for automatic API endpoint description generation

--- dog-api/src/dog/dog.controller.ts
+++ dog-api/src/dog/dog.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { DogService } from './dog.service';
 import { Dog } from '../entities/dog.entity';
-import { CreateDogDto } from './createDogDto';
+import { CreateDogDto } from './createDog.dto';
 
 @Controller('dogs')
 export class DogController {
   constructor(private readonly dogService: DogService) {}
 
+  /**
+   * Creates a Dog entity
+   */
   @Post()
   public create(@Body() createDogDto: CreateDogDto): Promise<Dog> {
     return this.dogService.create(createDogDto.dogSize);
