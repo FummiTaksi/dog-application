fe7628030aaaf93be70d3dbffc79dd5fef1928e3
Juuso Korhonen <juseek@gmail.com>
2022-02-10 16:17:59 +0200

    add generate script for orval, generate models and hooks

---
+++ react-query-test/generated/model/index.ts
@@ -0,0 +1,7 @@
+export * from './createDogDtoDogSize';
+export * from './addOwnerDto';
+export * from './human';
+export * from './dogSize';
+export * from './createHumanDto';
+export * from './createDogDto';
+export * from './dog';
